99125d6787673c5869a7f3394d691cf760a39aa3
karthik <karthiksathyan47@gmail.com>
2026-05-07 12:53:10 +0530

    first commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,11 +83,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       {
-        rel: "icon",
-        href: "/favicon.svg",
-        type: "image/svg+xml",
-      },
-      {
         rel: "stylesheet",
         href: appCss,
       },
